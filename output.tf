81e3bb31a3cc6991c584b1302b9a2906aff17d82
Delfim Marime <Delfimarime@gmail.com>
2025-01-11 15:47:07 +0200

    fix: substring replace by substr

--- output.tf
+++ output.tf
@@ -4,11 +4,11 @@ output "users" {
       for user in local.compute.user_roles : "${user.database_name}_${user.name}"
     ]) : {
       id       = key
-      database = substring(key, 0, index(key, "_"))
-      refers_to = substring(key, index(key, "_") + 1, length(key))
+      database = substr(key, 0, index(key, "_"))
+      refers_to = substr(key, index(key, "_") + 1, length(key))
       username = key
       password = random_password.users[
-      "${substring(key, 0, index(key, "_"))}/${substring(key, index(key, "_") + 1, length(key))}"
+      "${substr(key, 0, index(key, "_"))}/${substr(key, index(key, "_") + 1, length(key))}"
       ].result
     }
   ]
@@ -21,7 +21,7 @@ output "passwords" {
       for user in local.compute.user_roles : "${user.database_name}_${user.name}"
     ]) :
     key => random_password.users[
-    "${substring(key, 0, index(key, "_"))}/${substring(key, index(key, "_") + 1, length(key))}"
+    "${substr(key, 0, index(key, "_"))}/${substr(key, index(key, "_") + 1, length(key))}"
     ].result
   }
   sensitive = true
