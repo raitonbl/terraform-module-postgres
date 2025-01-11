a3f77e5d290b42b9208269424abea2ae7df43bef
Delfim Marime <Delfimarime@gmail.com>
2025-01-11 16:28:41 +0200

    refactor: rebuild module (#4)

--- output.tf
+++ output.tf
@@ -1,28 +1,21 @@
 output "users" {
-  value = [
-    for key in sort([
-      for user in local.compute.user_roles : "${user.database_name}_${user.name}"
-    ]) : {
-      id       = key
-      database = substring(key, 0, index(key, "_"))
-      refers_to = substring(key, index(key, "_") + 1, length(key))
-      username = key
-      password = random_password.users[
-      "${substring(key, 0, index(key, "_"))}/${substring(key, index(key, "_") + 1, length(key))}"
-      ].result
+  value = tomap({
+    for user in local.compute.user_roles :
+    "${user.database_name}_${user.name}" => {
+      refers_to = user.name
+      database  = user.database_name
+      username  = "${user.database_name}_${user.name}"
     }
-  ]
+  })
   sensitive = false
 }
 
 output "passwords" {
-  value = {
-    for key in sort([
-      for user in local.compute.user_roles : "${user.database_name}_${user.name}"
-    ]) :
-    key => random_password.users[
-    "${substring(key, 0, index(key, "_"))}/${substring(key, index(key, "_") + 1, length(key))}"
+  value = tomap({
+    for user in local.compute.user_roles :
+    "${user.database_name}_${user.name}" => random_password.users[
+    "${user.database_name}/${user.name}"
     ].result
-  }
+  })
   sensitive = true
 }
